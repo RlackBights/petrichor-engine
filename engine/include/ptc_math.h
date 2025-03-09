f852e3b2055dcd64cd429a82ff77baff9ec6f994
RlackBights <hunor.n.b@gmail.com>
2025-03-09 18:02:17 +0100

    Added *Math*, only easing functions for now

---
+++ engine/include/ptc_math.h
@@ -0,0 +1,46 @@
+#ifndef PTC_MATH_H
+#define PTC_MATH_H
+
+#include <glm/glm.hpp>
+#include <glm/ext/scalar_constants.hpp>
+
+enum EasingFunction {
+    IN_SINE,
+    OUT_SINE,
+    IN_OUT_SINE,
+    IN_QUAD,
+    OUT_QUAD,
+    IN_OUT_QUAD,
+    IN_CUBIC,
+    OUT_CUBIC,
+    IN_OUT_CUBIC,
+    IN_QUART,
+    OUT_QUART,
+    IN_OUT_QUART,
+    IN_QUINT,
+    OUT_QUINT,
+    IN_OUT_QUINT,
+    IN_EXPO,
+    OUT_EXPO,
+    IN_OUT_EXPO,
+    IN_CIRC,
+    OUT_CIRC,
+    IN_OUT_CIRC,
+    IN_BACK,
+    OUT_BACK,
+    IN_OUT_BACK,
+    IN_ELASTIC,
+    OUT_ELASTIC,
+    IN_OUT_ELASTIC,
+    IN_BOUNCE,
+    OUT_BOUNCE,
+    IN_OUT_BOUNCE
+};
+
+class Math
+{
+public:
+    static float ApplyEasing(float _x, EasingFunction _easingFunction);
+};
+
+#endif
