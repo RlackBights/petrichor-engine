767eec24eead5d5465f3569bd1ddae2bdb1cc362
RlackBights <hunor.n.b@gmail.com>
2025-03-17 14:42:21 +0100

    removed game related  stuff

--- scripts/game_main.cpp
+++ scripts/game_main.cpp
@@ -1,10 +1,1 @@
-#include "ptc_camera.h"
-#include "ptc_object.h"
-
-Object camera("camera"), text("text");
-
-void game_main()
-{
-    Camera* camComp = camera.AddComponent<Camera>(false, true);
-    camComp->backgroundColor = glm::vec4(0.1f, 0.1f, 0.1f, 1.0f);
-}
+void game_main() {}
